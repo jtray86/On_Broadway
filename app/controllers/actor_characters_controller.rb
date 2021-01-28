dac34546f3b17abb1d542df6ec642084a6afe06c
jennifer tracy <jenniferetracy@gmail.com>
2021-01-28 18:03:30 -0500

    authorization complete

--- app/controllers/actor_characters_controller.rb
+++ app/controllers/actor_characters_controller.rb
@@ -11,49 +11,47 @@ class ActorCharactersController < ApplicationController
     @actor_character = ActorCharacter.new
     
   end
+  
 
   def create
-    
-    
-    if params[:actor_character][:actor_id] && params[:actor_character][:character_id]
-        actor = params[:actor_character][:actor_id]
-        character = params[:actor_character][:character_id]
-        current = params[:actor_character][:current]
-               # byebug
-          if Actor.find_by(name: "#{actor}") == nil 
-            #||  Character.find_by(name: "#{character}") == nil
-            flash[:message] = "Invalid actor/character"
-            redirect_to new_actor_character_path(character)
-          else 
-            actor_id = Actor.find_by(name: "#{actor}").id 
-            #character_id = Character.find_by(name: "#{character}").id
-    
-            @actor_character = ActorCharacter.create(actor_id: actor_id, character_id: character, current: current)
-                if @actor_character.valid?
-                  currently_played_by
-                  redirect_to show_path(@actor_character.character.show)
-                else
-                  flash[:message] = @actor_character.errors.full_messages
-                  redirect_to new_actor_character_path
-                end
+    # set_current_user.shows.each do |show|
+    #   show.characters.each do |character|
+    #     if params[:actor_character][:character_id] == character.id
+            
+          if params[:actor_character][:actor_id] && params[:actor_character][:character_id]
+              actor = params[:actor_character][:actor_id]
+              character = params[:actor_character][:character_id]
+              current = params[:actor_character][:current]
+                  #  byebug
+                if Actor.find_by(name: "#{actor}") == nil 
+                  #||  Character.find_by(name: "#{character}") == nil
+                  flash[:message] = "Invalid actor/character"
+                  redirect_to new_actor_character_path(character)
+                else 
+                  actor_id = Actor.find_by(name: "#{actor}").id 
+                  #character_id = Character.find_by(name: "#{character}").id
           
+                  @actor_character = ActorCharacter.create(actor_id: actor_id, character_id: character, current: current)
+                      if @actor_character.valid?
+                        currently_played_by
+                        redirect_to show_path(@actor_character.character.show)
+                      else
+                        flash[:message] = @actor_character.errors.full_messages
+                        redirect_to new_actor_character_path
+                      end
+                end
+          else
+            redirect_to new_actor_character_path
           end
-
-    else
-      redirect_to new_actor_character_path
-    end
-
+      # else  
+      #   redirect_to admin_path(@current_user.id)
+        # end 
+    #   end
+    # end       
   end
 
   
-  # def edit
-  # end
-
-  # def update
-  #   actor_character = ActorCharacter.find(params[:id])
-  #   actor_character.update(actor_character_params(:actor_id, :character_id))
-  #   redirect_to show_path(actor_character.character.show)
-  # end
+  
   def currently_played_by
     character_current = @actor_character.character_id
     actor_current = @actor_character.actor_id
